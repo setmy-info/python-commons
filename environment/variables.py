b17268e42ad17fd9a789ba89d93e51f5994ebed0
Imre Tabur <imre.tabur@mail.ee>
2023-09-04 15:16:52 +0300

    Environment variables parsing

---
+++ environment/variables.py
@@ -0,0 +1,33 @@
+import os
+
+from strings.operations import trim_list, to_boolean, to_int, to_float
+
+
+def set_environment_variable(variable_name: str, variable_value: str):
+    os.environ[variable_name] = variable_value
+
+
+def get_environment_variable(variable_name: str):
+    return os.getenv(variable_name)
+
+
+def get_boolean_environment_variable(variable_name: str):
+    return to_boolean(get_environment_variable(variable_name))
+
+
+def get_int_environment_variable(variable_name: str):
+    return to_int(get_environment_variable(variable_name))
+
+
+def get_float_environment_variable(variable_name: str):
+    return to_float(get_environment_variable(variable_name))
+
+
+def get_environment_variables_list(variable_name: str, parse_function=None):
+    env_variable = get_environment_variable(variable_name)
+    if env_variable is None:
+        return []
+    trimmed_list = trim_list(env_variable.split(","))
+    if parse_function is not None:
+        return [parse_function(value, index) for index, value in enumerate(trimmed_list)]
+    return trimmed_list
---
+++ strings/operations.py
@@ -0,0 +1,33 @@
+def trim_list(strings_list):
+    fragments = [fragment.strip() for fragment in strings_list]
+    return fragments
+
+
+def to_boolean(text: str, default_value=False):
+    if text is None:
+        return default_value
+    lower_text = text.lower()
+    if lower_text == 'true' or lower_text == 'yes':
+        return True
+    elif lower_text == 'false' or lower_text == 'no':
+        return False
+    else:
+        raise ValueError("Invalid boolean value")
+
+
+def to_int(text: str, default_value=0):
+    if text is None:
+        return default_value
+    try:
+        return int(text)
+    except (ValueError, TypeError):
+        return None
+
+
+def to_float(text: str, default_value=0.0):
+    if text is None:
+        return default_value
+    try:
+        return float(text)
+    except (ValueError, TypeError):
+        return default_value
