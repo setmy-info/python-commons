1ea9a8b1152dd730af5bdb9bc43bba46444b7f62
Imre Tabur <imre.tabur@mail.ee>
2023-09-04 20:10:55 +0300

    String arrays multiplier

--- strings/operations.py
+++ strings/operations.py
@@ -1,4 +1,6 @@
 import json
+import re
+from itertools import product
 
 import yaml
 
@@ -54,3 +56,42 @@ def yaml_to_object(text: str, default_value={}):
         return yaml.safe_load(text)
     except (yaml.YAMLError):
         return None
+
+
+# TODO : test it
+def find_named_placeholders(text: str, to_clean: bool = True):
+    pattern = r"\${(.*?)}"
+    placeholders = re.findall(pattern, text)
+    if to_clean is True:
+        cleaned_placeholders = [placeholder.replace("${", "").replace("}", "") for placeholder in placeholders]
+        return cleaned_placeholders
+    return placeholders
+
+
+# TODO : test it
+def replace_named_placeholder(text: str, place_holder_name: str, replacement: str):
+    if replacement is None:
+        replacement_string = ""
+    else:
+        replacement_string = replacement
+    placeholder = "${" + place_holder_name + "}"
+    return text.replace(placeholder, replacement_string)
+
+
+# TODO : test it
+# list1 = ["A", "B", "C"]
+# list2 = ["X", "Y"]
+# result should be: ['AX', 'AY', 'BX', 'BY', 'CX', 'CY']
+def combined_list(list1: [str], list2: [str], join_text: str = ''):
+    """
+    Creates a new list by multiplying elements from two input lists, and joining them with the specified join_text separator.
+
+    Args:
+        list1 (list): The first input list.
+        list2 (list): The second input list.
+        join_text (str, optional): The separator used to join elements from the input lists. Defaults to an empty string.
+
+    Returns:
+        list: A new list where elements from the two input lists are multiplied and joined with the join_text separator.
+    """
+    return [join_text.join(items) for items in product(list1, list2)]
