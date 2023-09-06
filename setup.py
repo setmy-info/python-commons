d610d4b82a1acccfe1ba979aa3f197da91f9281c
Imre Tabur <imre.tabur@mail.ee>
2023-09-06 11:34:50 +0300

    Renamed project in setup.py

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup, find_packages
 
 setup(
-    name='python_commons',
+    name='smi_python_commons',
     version='0.1.0',
     description='setmy.info python commons library.',
     long_description='setmy.info python commons library.',
