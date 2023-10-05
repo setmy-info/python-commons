84d3a0377cd8f0894d03d2e3f53796489ffbdbe6
Imre Tabur <imre.tabur@mail.ee>
2023-10-05 22:16:52 +0300

    another

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='smi_python_commons',
-    version='0.2.3',
+    version='0.2.4',
     description='setmy.info python commons library.',
     long_description='setmy.info python commons library.',
     author='Imre Tabur',
