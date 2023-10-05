dae104a1076640120b98c44e1f1a60fe961c3918
Imre Tabur <imre.tabur@mail.ee>
2023-10-05 21:47:17 +0300

    Some probes

--- info/setmy/yaml/__init__.py
+++
@@ -1,3 +0,0 @@
-__all__ = ['parse_yaml_file']
-
-from info.setmy.yaml.parser import parse_yaml_file
--- setup.py
+++ setup.py
@@ -2,14 +2,14 @@ from setuptools import setup, find_packages
 
 setup(
     name='smi_python_commons',
-    version='0.2.1',
+    version='0.2.3',
     description='setmy.info python commons library.',
     long_description='setmy.info python commons library.',
     author='Imre Tabur',
     author_email='info@setmy.info',
     license='MIT',
     url='https://github.com/setmy-info/python-commons',
-    packages=find_packages(),
+    packages=find_packages(exclude=['test']),
     install_requires=[
         "pyyaml==6.0.1"
     ],
