36737dbc3864c125398d2fd8024bee02a89714e7
Imre Tabur <imre.tabur@mail.ee>
2023-10-06 08:32:23 +0300

    Probe

--- setup.py
+++ setup.py
@@ -2,14 +2,14 @@ from setuptools import setup, find_packages
 
 setup(
     name='smi_python_commons',
-    version='0.2.4',
+    version='0.2.5',
     description='setmy.info python commons library.',
     long_description='setmy.info python commons library.',
     author='Imre Tabur',
     author_email='info@setmy.info',
     license='MIT',
     url='https://github.com/setmy-info/python-commons',
-    packages=find_packages(exclude=['test']),
+    packages=find_packages(),
     install_requires=[
         "pyyaml==6.0.1"
     ],
